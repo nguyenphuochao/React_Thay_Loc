9a28674c6288fd5445a0e6f246d8c70916f7070a
Hảo Nguyễn <nguyenphuochao123@gmail.com>
2023-11-17 14:45:47 +0700

    m

---
+++ pure-redux-bank/script.js
@@ -0,0 +1,33 @@
+const { createStore } = window.Redux;
+// action là object vd: {type: 'increment'}
+const initialState = { money: 0 };
+const bankReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { money: state.money + action.payload }
+            break;
+        case 'decrement':
+            return { money: state.money - action.payload }
+            break;
+        default:
+            return state;
+    }
+}
+let store = createStore(bankReducer);
+store.subscribe(() => {
+    const state = store.getState();
+    const money = state.money;
+    var divTag = document.querySelector("#result");
+    divTag.innerHTML = money;
+});
+const buttonTags = document.querySelectorAll("button");
+for (const buttonTag of buttonTags) {
+    buttonTag.onclick = function () {
+        var type = buttonTag.getAttribute("data");
+        if (type == 'deposit') {
+            store.dispatch({ type: 'increment', payload: 10 });
+        } else {
+            store.dispatch({ type: 'decrement', payload: 10 });
+        }
+    }
+}
